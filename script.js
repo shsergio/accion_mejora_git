d4705959d8150af7a5a9db59881b16a3de6c9290
shsergio <sh04861@gmail.com>
2023-07-26 09:10:20 -0600

    Agregado el footer y la alerta de Hola mundo

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Inicialización del carrusel
-document.addEventListener('DOMContentLoaded', function() {
+// Alerta de "Hola mundo" al cargar la página
+window.addEventListener('load', function() {
+    alert('¡Hola mundo!');
+  });
+  
+  // Inicialización del carrusel
+  document.addEventListener('DOMContentLoaded', function() {
     var myCarousel = new bootstrap.Carousel(document.getElementById('carouselExample'), {
       interval: 5000, // Cambiar a la cantidad de milisegundos que deseas que cada slide se muestre antes de cambiar al siguiente.
       pause: 'hover' // Pausar el carrusel cuando se pase el mouse sobre él.
